ee22bf1f498ecf146d1c77a6f42a84af11ed22ac
ShcherbinaNick <scherbinanick@yandex.ru>
2021-10-12 17:20:02 +0300

    added js and new pops, fixed grid

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,148 @@
 // Переменная popup
-let popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup'); // позиционирование и скрытие попапа
+const profilePopup = document.querySelector('.popup_type_profile-edit'); // попап профиля
+const cardItemPopup = document.querySelector('.popup_type_new-card'); // попап создания карточки
+const imagePopup = document.querySelector('.popup_type_image'); // попап отображения картинки
 // Переменные кнопок
-let popupOpenBtn = document.querySelector('.profile__edit-button');
-let popupCloseBtn = popup.querySelector('.popup__close-button');
+const popupOpenBtn = document.querySelector('.profile__edit-button'); // редактирование профиля
+const popupCloseBtns = document.querySelectorAll('.popup__close-button'); // закрыть
+const addCard = document.querySelector('.profile__add-button'); // добавить карточку
 // Переменные полей профиля
-let profileName = document.querySelector('.profile__title');
-let profileDesc = document.querySelector('.profile__subtitle');
+const profileName = document.querySelector('.profile__title'); // название профиля
+const profileDesc = document.querySelector('.profile__subtitle'); // род деятельности профиля
 // Переменные формы
-let formElement = popup.querySelector('.popup__container');
-let nameInput = popup.querySelector('.popup__input-field-name');
-let jobInput = popup.querySelector('.popup__input-field-description');
+const profileFormElement = document.forms.profile_edit; // попап с редактированием информации пользователя
+const cardFormElement = document.forms.card_edit; // попап добавления новой карточки
+const nameInput = popup.querySelector('.popup__input-field-name'); // поле ввода профиля
+const jobInput = popup.querySelector('.popup__input-field-description'); // поле ввода рода деятельности
+// Переменные полей формы создания карточки
+const cardInputText = document.querySelector('.popup__input-field-card');
+const cardInputLink = document.querySelector('.popup__input-field-link');
+// Переменные для попаса с увеличенным изображением картинки
+const imagePopupPicture = imagePopup.querySelector('.popup__figure-image');
+const imagePopupTitle = imagePopup.querySelector('.popup__figure-title');
+// Массив из задания
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-// Открытие popup
-function openPopup() {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileDesc.textContent;
-  popup.classList.add('popup_active');
-};
+// Список карточек в разметке
+const cardsList = document.querySelector('.cards__list');
 
-// Закрытие popup
-function closePopup() {
-  popup.classList.remove('popup_active');
-};
+// Преобразование элементов массива
+function createCard(card) {
+  const cardsTemplate = document.querySelector('.cards-template');
+  const newCard = cardsTemplate.content.cloneNode(true);
+  const newCardName = newCard.querySelector('.card__name');
+  const newCardImage = newCard.querySelector('.card__image');
+  const likeBtn = newCard.querySelector('.card__like-button');
+  newCardName.textContent = card.name;
+  newCardImage.src = card.link;
+  // Лайк на каждую созданную карточку
+  likeBtn.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('card__like-button_active');
+  });
+  // Удаление карточки
+  const deleteCardBtn = newCard.querySelector('.card__delete');
+  deleteCardBtn.addEventListener('click', deleteCard);
+  // Открытие картинки
+  const fullImage = newCard.querySelector('.card__image');
+  fullImage.addEventListener('click', () => {
+    openImage(newCardImage.src, newCardImage.alt, newCardName.textContent);
+  });
 
-// Отслеживание событий клика открытия и закрытия окна
-popupOpenBtn.addEventListener('click', openPopup);
-popupCloseBtn.addEventListener('click', closePopup);
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                              // Так мы можем определить свою логику отправки.
-                                              // О том, как это делать, расскажем позже.
-
-  profileName.textContent = nameInput.value; 
-  profileDesc.textContent = jobInput.value;
-  closePopup();
-  // Получите значение полей jobInput и nameInput из свойства value
-
-  // Выберите элементы, куда должны быть вставлены значения полей
-
-  // Вставьте новые значения с помощью textContent
+  return newCard;
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
+function renderCards(item) {
+  const newCard = createCard(item);
+  cardsList.append(newCard);
+}
+
+initialCards.map(renderCards);
+
+// Открытие popup
+function openPopup(item) {
+  item.classList.add('popup_active');
+};
+
+popupOpenBtn.addEventListener('click', () => {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDesc.textContent;
+  openPopup(profilePopup);
+});
+
+// Закрытие попапов
+popupCloseBtns.forEach((item) => {
+  item.addEventListener('click', () => {
+    closePopup(item.closest('.popup'));
+  });
+});
+
+function closePopup(item) {
+  item.classList.remove('popup_active');
+};
+
+// Отвечает за открытие попапа с добавлением карточки
+addCard.addEventListener('click', () => {
+  openPopup(cardItemPopup);
+});
+
+// Добавление карточки при нажатии на кнопку
+function addNewCard(evt) {
+  evt.preventDefault();
+  const newCard = {
+    name: cardInputText.value,
+    link: cardInputLink.value
+  }
+  renderCards(newCard);
+  closePopup(cardItemPopup);
+  cardInputText.value = ''; // Очистка полей при закрытии
+  cardInputLink.value = ''; // Очистка полей при закрытии
+}
+cardFormElement.addEventListener('submit', addNewCard);
+
+// Лайк
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDesc.textContent = jobInput.value;
+  closePopup(profilePopup);
+}
+profileFormElement.addEventListener('submit', formSubmitHandler);
+
+// Удаление карточки
+function deleteCard(evt) {
+  evt.currentTarget.closest('.card').remove();
+}
+
+// Значения для попапа с увеличенным просмотром картинки
+function openImage(image, alt, figcaption) {
+imagePopupPicture.src = image;
+imagePopupPicture.alt = alt;
+imagePopupTitle.textContent = figcaption;
+openPopup(imagePopup);
+}
